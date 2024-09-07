93a7885192256aff75b4714b55228a22fcc508dd
Falon <47285315+falOn-Dev@users.noreply.github.com>
2024-09-07 11:48:02 -0400

    Addressable LED example in KT

---
+++ wpilibkExamples/src/main/kotlin/edu/wpi/first/wpilibk/examples/addressableled/Main.kt
@@ -0,0 +1,15 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package edu.wpi.first.wpilibk.examples.addressableled
+
+import edu.wpi.first.wpilibj.RobotBase
+import edu.wpi.first.wpilibk.examples.addressableled.Robot
+
+/**
+ * Do NOT add any static variables to this class, or any initialization at all. Unless you know what
+ * you are doing, do not modify this file except to change the parameter class to the startRobot
+ * call.
+ */
+fun main() = RobotBase.startRobot { Robot }
---
+++ wpilibkExamples/src/main/kotlin/edu/wpi/first/wpilibk/examples/addressableled/Robot.kt
@@ -0,0 +1,56 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package edu.wpi.first.wpilibk.examples.addressableled
+
+import edu.wpi.first.units.Distance
+import edu.wpi.first.units.Measure
+import edu.wpi.first.wpilibj.AddressableLED
+import edu.wpi.first.wpilibj.AddressableLEDBuffer
+import edu.wpi.first.wpilibj.LEDPattern
+import edu.wpi.first.wpilibj.TimedRobot
+import edu.wpi.first.units.Units.Meters
+import edu.wpi.first.units.Units.MetersPerSecond
+
+object Robot : TimedRobot() {
+    private val m_led: AddressableLED
+    private val m_ledBuffer: AddressableLEDBuffer
+
+    // Create an LED pattern that will display a rainbow across
+    // all hues at maximum saturation and half brightness
+    private val m_rainbow = LEDPattern.rainbow(255, 128)
+
+    // Our LED strip has a density of 120 LEDs per meter
+    private val kLedSpacing: Measure<Distance> = Meters.of(1 / 120.0)
+
+
+    // Create a new pattern that scrolls the rainbow pattern across the LED strip, moving at a speed
+    // of 1 meter per second.
+    private val m_scrollingRainbow = m_rainbow.scrollAtAbsoluteSpeed(MetersPerSecond.of(1.0), kLedSpacing)
+
+    /** Called once at the beginning of the robot program. */
+    init {
+        // PWM port 9
+        // Must be a PWM header, not MXP or DIO
+        m_led = AddressableLED(9)
+
+        // Reuse buffer
+        // Default to a length of 60, start empty output
+        // Length is expensive to set, so only set it once, then just update data
+        m_ledBuffer = AddressableLEDBuffer(60)
+        m_led.setLength(m_ledBuffer.length)
+
+        // Set the data
+        m_led.setData(m_ledBuffer)
+        m_led.start()
+    }
+
+    override fun robotPeriodic() {
+        // Update the buffer with the rainbow animation
+        m_scrollingRainbow.applyTo(m_ledBuffer)
+        // Set the LEDs
+        m_led.setData(m_ledBuffer)
+    }
+}
+
